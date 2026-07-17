fe14150f563f9a1fc04fb8aaced378c593ad4969
Evebreeze <136620890+chincika@users.noreply.github.com>
2026-07-17 14:25:58 +0800

    feat: upload local news images with hot updates

--- apps/maintainer/src/preload/index.ts
+++ apps/maintainer/src/preload/index.ts
@@ -1,6 +1,7 @@
 import { contextBridge, ipcRenderer } from "electron";
 const maintainerApi = Object.freeze({
   chooseDirectory: () => ipcRenderer.invoke("content:choose-directory") as Promise<string | null>,
+  chooseNewsImage: () => ipcRenderer.invoke("content:choose-news-image") as Promise<{ imageFile: string; width: number; height: number; sizeBytes: number } | null>,
   load: (directory?: string) => ipcRenderer.invoke("content:load", directory) as Promise<any>,
   saveRecord: (input: any) => ipcRenderer.invoke("content:save-record", input) as Promise<any>,
   deleteRecord: (input: any) => ipcRenderer.invoke("content:delete-record", input) as Promise<any>,
